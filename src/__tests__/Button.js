c3dcfd1a9a6faa4d3a539cb8610b38bb070ec18f
milliampere <millla@hotmail.com>
2018-01-22 12:28:50 +0100

    Danger style test.

---
+++ src/__tests__/Button.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import Button from '../components/Button';
+
+it('should display the correct danger style', () => {
+  const dangerStyle = 'bg-red-dark hover:bg-red-darker text-white font-bold py-2 px-4 rounded-full';
+  const wrapper = mount(<Button onClick={() => {}} danger >Button</Button>);
+  expect(wrapper.find("[data-test='button']").hasClass(dangerStyle)).toBeTruthy()
+});
