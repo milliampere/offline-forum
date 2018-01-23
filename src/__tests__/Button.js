08e23f3e0edb445f54e386bfffb892f77c48a629
milliampere <millla@hotmail.com>
2018-01-23 09:57:31 +0100

    Add describe.

--- src/__tests__/Button.js
+++ src/__tests__/Button.js
@@ -2,8 +2,10 @@ import React from 'react';
 import { mount } from 'enzyme';
 import Button from '../components/Button';
 
-it('should display the correct danger style', () => {
-  const dangerStyle = 'bg-red-dark hover:bg-red-darker text-white font-bold py-2 px-4 rounded-full';
-  const wrapper = mount(<Button onClick={() => {}} danger >Button</Button>);
-  expect(wrapper.find("[data-test='button']").hasClass(dangerStyle)).toBeTruthy()
+describe('<Button />', () => {
+  it('should display the correct danger style', () => {
+    const dangerStyle = 'bg-red-dark hover:bg-red-darker text-white font-bold py-2 px-4 rounded-full';
+    const wrapper = mount(<Button onClick={() => {}} danger >Button</Button>);
+    expect(wrapper.find("[data-test='button']").hasClass(dangerStyle)).toBeTruthy()
+  });
 });
