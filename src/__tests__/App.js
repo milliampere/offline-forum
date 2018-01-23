ca2c144245bcc8132b863625d4a18169f90fc17d
milliampere <millla@hotmail.com>
2018-01-23 15:37:56 +0100

    Added App tests.

--- src/__tests__/App.js
+++ src/__tests__/App.js
@@ -1,7 +1,27 @@
 import React from 'react';
-import { render } from 'enzyme';
+import { render, mount } from 'enzyme';
 import App from '../components/App';
 
 test('renders the app', () => {
   render(<App />);
+});
+
+it('should change page from home to bot', () => {
+  const wrapper = mount(<App />);
+  wrapper.setState({ currentPage: 'home' });
+  wrapper.instance().changePage();
+  expect(wrapper.state('currentPage')).toBe('bot');
+});
+
+it('should change page from bot to home', () => {
+  const wrapper = mount(<App />);
+  wrapper.setState({ currentPage: 'bot' });
+  wrapper.instance().changePage();
+  expect(wrapper.state('currentPage')).toBe('home');
+});
+
+it('should change persona state', () => {
+  const wrapper = mount(<App />);
+  wrapper.instance().changePersona({target: { value: 'morgana'}});
+  expect(wrapper.state('currentPersona')).toBe('morgana');
 });
