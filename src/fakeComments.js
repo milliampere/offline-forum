ba04151daca11a85801179705fee32a4f3f53c95
milliampere <millla@hotmail.com>
2018-01-25 11:11:40 +0100

    Added fake comments.

---
+++ src/fakeComments.js
@@ -0,0 +1,25 @@
+export default {
+  data: [
+    {
+      id: '_f3io6nfiv',
+      postId: '565ddy34',
+      comment: 'Nice!',
+      author: 'Zac',
+      date: (new Date()).toLocaleString()
+    },
+    {
+      id: '_mn6l1ezir',
+      postId: '45sfdf56',
+      comment: 'Awesome!',
+      author: 'Esmeralda',
+      date: (new Date()).toLocaleString()
+    },
+    {
+      id: '_9gdxesv86',
+      postId: '45sfdf56',
+      comment: 'Cool!',
+      author: 'Zac',
+      date: (new Date()).toLocaleString()
+    }
+  ]
+};
