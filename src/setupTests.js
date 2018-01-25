f72cd49c31cd7a603ac91a914bcd652727241100
milliampere <millla@hotmail.com>
2018-01-25 11:11:20 +0100

    Added localStorageMock.

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,10 +3,24 @@ import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
 
-const localStorageMock = {
-  getItem: jest.fn(),
-  setItem: jest.fn(),
-  clear: jest.fn()
-};
+/**
+ * Extended version of localStorage mock, this is 
+ * so you can actually save stuff in localStorage
+ */
+const localStorageMock = (() => {
+  let store = {}
+  return {
+    getItem: jest.fn(key => store[key]),
+    setItem: jest.fn((key, value) => {
+      store[key] = value.toString();
+    }),
+    clear: jest.fn(() => {
+      store = {}
+    }),
+    removeItem: jest.fn(key => {
+      delete store[key];
+    })
+  }
+});
 
-global.localStorage = localStorageMock;
+global.localStorage = localStorageMock()
