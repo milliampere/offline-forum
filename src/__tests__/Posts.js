ed8313b91a43c8507f231a00085b286caa2477d9
milliampere <millla@hotmail.com>
2018-01-31 18:03:04 +0100

    Refactor.

--- src/__tests__/Posts.js
+++ src/__tests__/Posts.js
@@ -6,31 +6,40 @@ describe('<Posts />', () => {
 
   let wrapper;
 
-  const mockPost = [
+  const mockPosts = [
     {
       id: '1', 
       title: 'title',
       content: 'content',
       author: 'test',
       date: '2018-01-25 13:37:00'
+    },
+    {
+      id: '2', 
+      title: 'title',
+      content: 'content',
+      author: 'test',
+      date: '2018-01-24 13:37:00'
     }
   ]
 
   beforeEach(() => {
-    localStorage.clear();
-    jest.resetModules();
-    localStorage.setItem('posts', JSON.stringify(mockPost));
+    localStorage.setItem('posts', JSON.stringify(mockPosts));
     wrapper = mount(<Posts currentPersona="test" />);
   })
 
-  it('should render a post', () => {
-    expect(wrapper.find('SinglePost').length).toEqual(1);
+  afterEach(() => {
+    localStorage.clear();
+  });
+
+  it('should render posts', () => {
+    expect(wrapper.find('SinglePost').length).toEqual(2);
   })
 
   it('should remove a post', () => {
-    expect(wrapper.state('posts')).toHaveLength(1);
+    expect(wrapper.state('posts')).toHaveLength(2);
     wrapper.instance().removePost('1');
-    expect(wrapper.state('posts')).toHaveLength(0);
+    expect(wrapper.state('posts')).toHaveLength(1);
   })
 
 })
