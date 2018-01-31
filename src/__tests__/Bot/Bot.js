b7253113aa3a152a13ee50e20fe50e2d6a507635
milliampere <millla@hotmail.com>
2018-01-31 21:26:24 +0100

    Refactor.

--- src/__tests__/Bot/Bot.js
+++ src/__tests__/Bot/Bot.js
@@ -2,30 +2,15 @@ import React from 'react';
 import { shallow, mount } from 'enzyme';
 import Bot from '../../components/Bot/Bot';
 
-function flushPromises() {
-  return new Promise(resolve => setImmediate(resolve));
-};
-
 describe('<Bot/>', () => {
 
-  it('should put onsubmit message in state', () => {
+  it('should put user message in state', () => {
     const wrapper = shallow(<Bot />);
     wrapper.instance().onSubmit('Hello');
     expect(wrapper.state().messages[0]).toEqual(expect.objectContaining({
       message: 'Hello',
+      bot: false
     }));
-  })
-
-  it('should put reply message in state', () => {
-    jest.useFakeTimers();
-    const wrapper = mount(<Bot />);
-    wrapper.instance().sendReply();
-    jest.runAllTimers();
-    flushPromises().then(() => {
-      expect(wrapper.state().messages[0]).toEqual(expect.objectContaining({
-        message: expect.any(String)
-      }));
-    });
   })
 
   it('should render a message', ()=> {
@@ -33,6 +18,34 @@ describe('<Bot/>', () => {
     wrapper.instance().onSubmit('Hello, how are you?');
     expect(wrapper.html()).toContain('Hello, how are you?');
   });
+
+
+})
+
+describe('bot reply integration', () => {
+
+  function flushAllPromises() {
+    return new Promise(resolve => setImmediate(resolve));
+  }
+
+  const wrapper = mount(<Bot />);
+  jest.useFakeTimers();
+  wrapper.instance().sendReply();
+  jest.runAllTimers();
+
+  it('should get reply from api and put it in state', () => {
+    return flushAllPromises().then(() => {
+      expect(wrapper.state().messages[0]).toEqual(expect.objectContaining({
+        message: expect.any(String),
+        bot: true
+      }));
+    });
+  })
+
+  it('should render reply', () => {
+    const reply = wrapper.state().messages[0].message;
+    expect(wrapper.html()).toContain(reply);
+  })
 
 
 })
