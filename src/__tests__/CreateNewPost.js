6d0912ac85ad16913147363312d747bd6120a7f4
milliampere <millla@hotmail.com>
2018-01-31 19:21:56 +0100

    Refactor.

--- src/__tests__/CreateNewPost.js
+++ src/__tests__/CreateNewPost.js
@@ -2,37 +2,54 @@ import React from 'react';
 import { mount } from 'enzyme';
 import CreateNewPost from '../components/CreateNewPost';
 
-describe('<CreateNewPost />', () => {
-  
-  let fakeUpdateFunction; 
-  let wrapper;
+const fakeUpdateFunction = jest.fn(); 
+let wrapper;
 
-  beforeEach(() => {
-    localStorage.clear();
-    fakeUpdateFunction = jest.fn();
-    wrapper = mount(<CreateNewPost author="test" updatePosts={fakeUpdateFunction} />);
-  });
+describe('onchange tests', () => {
 
-  it('should change state on input onchange', () => {
+  wrapper = mount(<CreateNewPost author="test" updatePosts={fakeUpdateFunction} />);
+
+  it('should change state on title onchange', () => {
     expect(wrapper.state('title')).toBe('');
-    wrapper.instance().onChange({target: {value: 'My title', name: 'title'}})
+    wrapper.find('#title').simulate('change', {target: {value: 'My title', name: 'title'}})
     expect(wrapper.state('title')).toBe('My title');
   }); 
 
-  it('should call updatePosts', () => {
-    wrapper.setState({title: 'Titel', content: 'Content'});
-    wrapper.find('form').simulate('submit');
-    expect(fakeUpdateFunction).toHaveBeenCalled();
+  it('should change state on content onchange', () => {
+    expect(wrapper.state('content')).toBe('');
+    wrapper.find('#content').simulate('change', {target: {value: 'My content', name: 'content'}})
+    expect(wrapper.state('content')).toBe('My content');
   });
 
-  it('should set post in local storage', () => {
-    wrapper.setState({title: 'Titel', content: 'Content'});
+})
+
+describe('submit tests', () => {
+  
+  beforeEach(() => {
+    wrapper = mount(<CreateNewPost author="test" updatePosts={fakeUpdateFunction} />);
+    wrapper.setState({title: 'TestTitle', content: 'TestContent'});
     wrapper.find('form').simulate('submit');
-    const posts = JSON.parse(localStorage.getItem('posts'));
-    expect(posts).toEqual(expect.arrayContaining([expect.objectContaining({
-      title: 'Titel',
-      content: 'Content'
-    })]));
   });
+
+  afterEach(() => {
+    localStorage.clear();
+  })
+
+  it('should call updatePosts', () => {
+    expect(fakeUpdateFunction).toHaveBeenCalledTimes(1);
+  });
+
+  it('should put post in local storage', () => {
+    const posts = JSON.parse(localStorage.getItem('posts'));
+    expect(posts[0]).toEqual(expect.objectContaining({
+      title: 'TestTitle',
+      content: 'TestContent'
+    }));
+  });
+
+  it('should clear form after submit', () => {
+    expect(wrapper.state('title')).toBe('');
+    expect(wrapper.state('content')).toBe('');
+  })
 
 });
