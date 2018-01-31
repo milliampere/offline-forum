3e9ab9163adbbcbc9c5f151541eb059ce226db29
milliampere <millla@hotmail.com>
2018-01-31 21:24:47 +0100

    Added snapshots.

--- src/__tests__/Button.js
+++ src/__tests__/Button.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import Button from '../components/Button';
 
 describe('<Button />', () => {
   it('should display the correct danger style', () => {
     const dangerStyle = 'bg-red-dark hover:bg-red-darker text-white font-bold py-2 px-4 rounded-full';
-    const wrapper = mount(<Button onClick={() => {}} danger >Button</Button>);
+    const wrapper = shallow(<Button onClick={() => {}} danger >Button</Button>);
     expect(wrapper.find("[data-test='button']").hasClass(dangerStyle)).toBeTruthy()
   });
+
+  it('should match snapshots', () => {
+    let wrapper = shallow(<Button onClick={() => {}}>Button</Button>);
+    expect(wrapper).toMatchSnapshot("Default style");
+    wrapper = shallow(<Button onClick={() => {}} danger>Button</Button>);
+    expect(wrapper).toMatchSnapshot("Danger style");
+  })
 });
+
