75694e1ca2573d8ab589bb149ed2be33fb9cd49e
milliampere <millla@hotmail.com>
2018-01-31 22:05:57 +0100

    Away you snapshot.

--- src/__tests__/SinglePost.js
+++ src/__tests__/SinglePost.js
@@ -4,41 +4,37 @@ import SinglePost from '../components/SinglePost';
 
 describe('<SinglePost />', () => {
 
-  let handleClick;
-  let wrapper;
-
-  beforeEach(() => {
-    handleClick = jest.fn();
-    wrapper = mount(
-      <SinglePost 
-        title="TestTitle" 
-        content="TestContent" 
-        id="123" 
-        author="TestPerson" 
-        currentPersona="TestPerson" 
-        date="2018-01-01" 
-        onClick={handleClick}
-      />);
-  });
+  const handleClick = jest.fn();
+  const wrapper = mount(
+    <SinglePost 
+      title="TestTitle" 
+      content="TestContent" 
+      id="123" 
+      author="TestPerson" 
+      currentPersona="TestPerson" 
+      date="2018-01-01" 
+      onClick={handleClick} 
+    />
+  );
 
   it('should display button when author === persona', () => {
     expect(wrapper.find('Button').length).toEqual(1);
   })
 
+  it('should call click with id', () => {
+    expect(handleClick).toHaveBeenCalledTimes(0);
+    wrapper.find('[data-test="button"]').first().simulate('click');
+    expect(handleClick).toHaveBeenCalledWith('123');
+  }) 
+
   it('should not display button when author !== persona', () => {
-    wrapper.setProps({ author: 'OnePerson' });
     wrapper.setProps({ currentPersona: 'AnotherPerson' });
     expect(wrapper.find('Button').length).toEqual(0);
   })
 
-  it('should call click with id', () => {
-    expect(handleClick).toHaveBeenCalledTimes(0);
-    wrapper.find('[data-test="button"]').simulate('click');
-    expect(handleClick).toHaveBeenCalledWith('123');
-  }) 
-
-  it('should render the post', () => {
-    expect(wrapper).toMatchSnapshot();
+  it('should render the post title and content', () => {
+    expect(wrapper.find('article h2').text()).toEqual('TestTitle');
+    expect(wrapper.find('article div p').text()).toEqual('TestContent');
   })
 
 })
