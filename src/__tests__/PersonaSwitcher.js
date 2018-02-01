2259bf3fe1f79e81128c61bd85f31b4fdf9d39ba
milliampere <millla@hotmail.com>
2018-02-01 10:45:58 +0100

    Added a not so important test.

--- src/__tests__/PersonaSwitcher.js
+++ src/__tests__/PersonaSwitcher.js
@@ -2,9 +2,15 @@ import React from 'react';
 import { mount } from 'enzyme';
 import PersonaSwitcher from '../components/PersonaSwitcher';
 
-it('should handle the onchange event', () => {
+it('should call change function', () => {
   const handleChange = jest.fn();
   const wrapper = mount(<PersonaSwitcher currentPersona="zac" changePersona={handleChange} />);
   wrapper.find('select').simulate('change', {target: {value: 'morgana'}});
   expect(handleChange).toHaveBeenCalledTimes(1);
+});
+
+it('should render current persona as value in select', () => {
+  const handleChange = jest.fn();
+  const wrapper = mount(<PersonaSwitcher currentPersona="test" changePersona={handleChange} />);
+  expect(wrapper.find('select').props().value).toEqual('test');  
 });
